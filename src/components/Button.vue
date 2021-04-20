95382525bd5700b8c71a2cd69b725b3adf2165db
ElnuDev <9874955+ElnuDev@users.noreply.github.com>
2021-04-19 18:43:44 -0700

    Create Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="$emit('click')" class="btn btn-primary">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: {
+      type: String,
+      default: "Button",
+    },
+  },
+};
+</script>
