5a87f007be7bf76de4031796e310ac552a092048
ElnuDev <9874955+ElnuDev@users.noreply.github.com>
2021-04-19 18:46:52 -0700

    Add vuex to main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import { createStore } from "vuex";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
-createApp(App).mount('#app');
+createApp(App).use(createStore({})).mount("#app");
